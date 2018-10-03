c40cd6648d4dd5a65b2d15bfddbfa65cd5afbb87
ProjetoCNEC <42986008+ProjetoCNEC@users.noreply.github.com>
2018-10-02 22:37:57 -0300

    Classe FalarController- Controler / Falar-Model

---
+++ jericho/Jericho.Controller/FalarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jericho.Model;
+
+namespace Jericho.Controller
+{
+    public static class FalarController
+    {
+        private static Falar Falar = new Falar();
+
+        public static List<string> Vozes()
+        {
+            return Falar.Vozes();
+        }
+
+        public static void FalarNumero(string voz, string numero)
+        {
+            Falar.FalarNumero(voz, numero);
+        }
+    }
+}
---
+++ jericho/Jericho.Model/Falar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Jericho.Model
+{
+    public class Falar
+    {
+        SpeechSynthesizer voz;
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+
+
+        public Falar()
+        {
+            voz = new SpeechSynthesizer();
+        }
+        public List<string> Vozes()
+        {
+            List<string> listaDeVozes = new List<string>();
+            foreach (var vozes in voz.GetInstalledVoices())
+            {
+                listaDeVozes.Add(vozes.VoiceInfo.Name);
+            }
+            return listaDeVozes;
+        }
+
+
+        public void FalarNumero(string voz, string numero)
+        {
+            speech = new SpeechSynthesizer();
+            speech.SelectVoice(voz);
+            speech.SetOutputToDefaultAudioDevice();
+            speech.Rate = 0;
+            speech.SpeakAsync(numero);
+        }
+    }
+}
