5d45765c8e238e552ee2e32f98f54f6e791ad3f5
Dieiverson <Dieiverson@gmail.com>
2018-10-16 19:56:02 -0300

    Classes Dao

--- jericho/Jericho.Model/Ebook.cs
+++ jericho/Jericho.Model/Ebook.cs
@@ -8,99 +8,14 @@ namespace Jericho.Model
 {
     public class Ebook
     {
-        string idEbook, autor, Editora, NomeLivro, CaminhoEbook, Sinopse;
-        DateTime dataAdd;
-
-        public string IdEbook
-        {
-            get
-            {
-                return idEbook;
-            }
-
-            set
-            {
-                idEbook = value;
-            }
-        }
-
-        public string Autor
-        {
-            get
-            {
-                return autor;
-            }
-
-            set
-            {
-                autor = value;
-            }
-        }
-
-        public string Editora1
-        {
-            get
-            {
-                return Editora;
-            }
-
-            set
-            {
-                Editora = value;
-            }
-        }
-
-        public string NomeLivro1
-        {
-            get
-            {
-                return NomeLivro;
-            }
-
-            set
-            {
-                NomeLivro = value;
-            }
-        }
-
-        public string CaminhoEbook1
-        {
-            get
-            {
-                return CaminhoEbook;
-            }
-
-            set
-            {
-                CaminhoEbook = value;
-            }
-        }
-
-        public string Sinopse1
-        {
-            get
-            {
-                return Sinopse;
-            }
-
-            set
-            {
-                Sinopse = value;
-            }
-        }
-
-        public DateTime DataAdd
-        {
-            get
-            {
-                return dataAdd;
-            }
-
-            set
-            {
-                dataAdd = value;
-            }
-        }
+       
+        public string idEbook { get; set; }
+        public string autor { get; set; }
+        public string Editora { get; set; }
+        public string NomeLivro { get; set; }
+        public string CaminhoEbook { get; set; }
+        public string Sinopse { get; set; }
+        public DateTime dataAdd { get; set; }       
 
         public Ebook()
         {
@@ -109,13 +24,13 @@ namespace Jericho.Model
 
         public Ebook(string idEbook, string autor, string editora, string nomeLivro, string caminhoEbook, string sinopse, DateTime dataAdd)
         {
-            this.IdEbook = idEbook;
-            this.Autor = autor;
-            Editora1 = editora;
-            NomeLivro1 = nomeLivro;
-            CaminhoEbook1 = caminhoEbook;
-            Sinopse1 = sinopse;
-            this.DataAdd = dataAdd;
+            this.idEbook = idEbook;
+            this.autor = autor;
+            Editora = editora;
+            NomeLivro = nomeLivro;
+            CaminhoEbook = caminhoEbook;
+            Sinopse = sinopse;
+            this.dataAdd = dataAdd;
         }
     }
 }
--- jericho/Jericho.Model/EbookDao.cs
+++ jericho/Jericho.Model/EbookDao.cs
@@ -17,34 +17,51 @@ namespace Jericho.Model
        public Ebook GetEbook(string _id)
             {
                 DataBase db = new DataBase();
+            try {
+                db.OpenConnection();
                 SQLiteDataReader dr;
                 db.AddParameter("@id", _id, DbType.String);
                 dr = db.ExecuteReader("select * from Ebook WHERE Id_Ebook = @id");
-                Ebook ebook = new Ebook();                if (dr.Read())
+                Ebook ebook = new Ebook(); if (dr.Read())
                 {
-                    ebook.Autor = Convert.ToString(dr["Autor"]);
-                    ebook.CaminhoEbook1 = Convert.ToString(dr["CaminhoEbook"]);
-                    ebook.DataAdd = Convert.ToDateTime(dr["DataAdicionado"]);
-                    ebook.Editora1 = Convert.ToString(dr["Editora"]);
-                    ebook.Sinopse1 = Convert.ToString(dr["sinopse"]);
-                    ebook.IdEbook = Convert.ToString(dr["Id_Ebook"]);
-                    ebook.NomeLivro1 = Convert.ToString(dr["NomeLivro"]);
+                    ebook.autor = Convert.ToString(dr["Autor"]);
+                    ebook.CaminhoEbook = Convert.ToString(dr["CaminhoEbook"]);
+                    ebook.dataAdd = Convert.ToDateTime(dr["DataAdicionado"]);
+                    ebook.Editora = Convert.ToString(dr["Editora"]);
+                    ebook.Sinopse = Convert.ToString(dr["sinopse"]);
+                    ebook.idEbook = Convert.ToString(dr["Id_Ebook"]);
+                    ebook.NomeLivro = Convert.ToString(dr["NomeLivro"]);
                 }
                 return ebook;
             }
+            catch(Exception ex)
+            {
+                return null;
+                ex.ToString();
+            }
+            finally
+            {
+                db.CloseConection();
+            }
+       }
         public int InsertEbook(Ebook ebook)
         {
             DataBase db = new DataBase();
-            db.AddParameter("@id", ebook.IdEbook, DbType.String);
-            db.AddParameter("@Autor", ebook.Autor, DbType.String);
-            db.AddParameter("@CaminhoEbook", ebook.CaminhoEbook1, DbType.String);
-            db.AddParameter("@Editora", ebook.Editora1, DbType.String);
-            db.AddParameter("@sinopse", ebook.Sinopse1, DbType.String);
-            db.AddParameter("@NomeLivro", ebook.NomeLivro1, DbType.String);
-            db.AddParameter("@DataAdicionado", ebook.DataAdd, DbType.DateTime);
+            try {
+                db.AddParameter("@id", ebook.idEbook, DbType.String);
+                db.AddParameter("@Autor", ebook.autor, DbType.String);
+                db.AddParameter("@CaminhoEbook", ebook.CaminhoEbook, DbType.String);
+                db.AddParameter("@Editora", ebook.Editora, DbType.String);
+                db.AddParameter("@sinopse", ebook.Sinopse, DbType.String);
+                db.AddParameter("@NomeLivro", ebook.NomeLivro, DbType.String);
+                db.AddParameter("@DataAdicionado", ebook.dataAdd, DbType.DateTime);
 
-            return db.ExecuteNoQuery("Insert into Ebook(Id_Ebook,Autor,CaminhoEbook,Editora,sinopse,NomeLivro,DataAdicionado) Values(@id,@Autor,@CaminhoEbook,@Editora,@sinopse,@NomeLivro,@DataAdicionado)");
+                return db.ExecuteNoQuery("Insert into Ebook(Id_Ebook,Autor,CaminhoEbook,Editora,sinopse,NomeLivro,DataAdicionado) Values(@id,@Autor,@CaminhoEbook,@Editora,@sinopse,@NomeLivro,@DataAdicionado)");
+            }
+            catch(Exception ex)
+            {
 
+            }
         }
         public int DeleteMp3(string _id)
         {
@@ -56,15 +73,52 @@ namespace Jericho.Model
         public int UpdateEbook(Ebook ebook)
         {
             DataBase db = new DataBase();
-            db.AddParameter("@id", ebook.IdEbook, DbType.String);
-            db.AddParameter("@Autor", ebook.Autor, DbType.String);
-            db.AddParameter("@CaminhoEbook", ebook.CaminhoEbook1, DbType.String);
-            db.AddParameter("@Editora", ebook.Editora1, DbType.String);
-            db.AddParameter("@sinopse", ebook.Sinopse1, DbType.String);
-            db.AddParameter("@NomeLivro", ebook.NomeLivro1, DbType.String);
-            db.AddParameter("@DataAdicionado", ebook.DataAdd, DbType.DateTime);
-            return db.ExecuteNoQuery("Insert Ebook SET (Id_Ebook,Autor,CaminhoEbook,Editora,sinopse,NomeLivro,DataAdicionado) Where Id_Ebook = @id" );
+            try
+            {
+                
+                db.OpenConnection();
+                db.AddParameter("@id", ebook.idEbook, DbType.String);
+                db.AddParameter("@Autor", ebook.autor, DbType.String);
+                db.AddParameter("@CaminhoEbook", ebook.CaminhoEbook, DbType.String);
+                db.AddParameter("@Editora", ebook.Editora, DbType.String);
+                db.AddParameter("@sinopse", ebook.Sinopse, DbType.String);
+                db.AddParameter("@NomeLivro", ebook.NomeLivro, DbType.String);
+                db.AddParameter("@DataAdicionado", ebook.dataAdd, DbType.DateTime);
+                return db.ExecuteNoQuery("Insert Ebook SET (Id_Ebook,Autor,CaminhoEbook,Editora,sinopse,NomeLivro,DataAdicionado) Where Id_Ebook = @id");
+            }
+            catch (Exception ex)
+            {
+                return 0;
+                ex.ToString();
+                
+            }
+            finally
+            {
+               db.CloseConection();
+            }
+            
         }
-
+        public List<Ebook> GetAll()
+        {
+            DataBase db = new DataBase();
+            SQLiteDataReader dr;
+           dr = db.ExecuteReader("select * from Ebook");
+            List<Ebook> ebooks = new List<Ebook>();
+            Ebook ebook = new Ebook();
+            while (dr.Read())
+            {
+                ebook.autor = Convert.ToString(dr["Autor"]);
+                ebook.CaminhoEbook = Convert.ToString(dr["CaminhoEbook"]);
+                ebook.dataAdd = Convert.ToDateTime(dr["DataAdicionado"]);
+                ebook.Editora = Convert.ToString(dr["Editora"]);
+                ebook.Sinopse = Convert.ToString(dr["sinopse"]);
+                ebook.idEbook = Convert.ToString(dr["Id_Ebook"]);
+                ebook.NomeLivro = Convert.ToString(dr["NomeLivro"]);
+                ebooks.Add(ebook);
+            }
+            return ebooks;
+        }
     }
+
 }
+
