acfaff6ace05a13a0981791d0b3f10e98955a46f
colegio <t.informatica00@gmail.com>
2018-11-06 19:36:37 -0200

    alteração tela

--- jericho/Jericho/Player_Mp3.Designer.cs
+++ jericho/Jericho/Player_Mp3.Designer.cs
@@ -30,30 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player_Mp3));
-            this.btn_Play = new System.Windows.Forms.Button();
             this.Player = new AxWMPLib.AxWindowsMediaPlayer();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.btn_pause = new System.Windows.Forms.Button();
-            this.btn_stop = new System.Windows.Forms.Button();
-            this.btn_volumeMais = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.btn_volumeMenos = new System.Windows.Forms.Button();
+            this.btn_volumeMais = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_Play = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btn_Play
-            // 
-            this.btn_Play.Location = new System.Drawing.Point(141, 166);
-            this.btn_Play.Name = "btn_Play";
-            this.btn_Play.Size = new System.Drawing.Size(69, 48);
-            this.btn_Play.TabIndex = 0;
-            this.btn_Play.Text = "Play";
-            this.btn_Play.UseVisualStyleBackColor = true;
-            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
             // 
             // Player
             // 
             this.Player.Enabled = true;
-            this.Player.Location = new System.Drawing.Point(128, 79);
+            this.Player.Location = new System.Drawing.Point(78, 79);
             this.Player.Name = "Player";
             this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
             this.Player.Size = new System.Drawing.Size(220, 42);
@@ -66,51 +57,80 @@
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
-            // btn_pause
+            // button1
             // 
-            this.btn_pause.Location = new System.Drawing.Point(226, 166);
-            this.btn_pause.Name = "btn_pause";
-            this.btn_pause.Size = new System.Drawing.Size(69, 48);
-            this.btn_pause.TabIndex = 2;
-            this.btn_pause.Text = "Pause";
-            this.btn_pause.UseVisualStyleBackColor = true;
-            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
-            // 
-            // btn_stop
-            // 
-            this.btn_stop.Location = new System.Drawing.Point(313, 166);
-            this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(69, 48);
-            this.btn_stop.TabIndex = 3;
-            this.btn_stop.Text = "stop";
-            this.btn_stop.UseVisualStyleBackColor = true;
-            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
-            // 
-            // btn_volumeMais
-            // 
-            this.btn_volumeMais.Location = new System.Drawing.Point(141, 248);
-            this.btn_volumeMais.Name = "btn_volumeMais";
-            this.btn_volumeMais.Size = new System.Drawing.Size(69, 48);
-            this.btn_volumeMais.TabIndex = 4;
-            this.btn_volumeMais.Text = "Volume +";
-            this.btn_volumeMais.UseVisualStyleBackColor = true;
-            this.btn_volumeMais.Click += new System.EventHandler(this.btn_volumeMais_Click);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Jericho.Properties.Resources.Google_Play_Music_icon;
+            this.button1.Location = new System.Drawing.Point(471, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(432, 374);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // btn_volumeMenos
             // 
-            this.btn_volumeMenos.Location = new System.Drawing.Point(226, 248);
+            this.btn_volumeMenos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_volumeMenos.ForeColor = System.Drawing.Color.Gold;
+            this.btn_volumeMenos.Image = global::Jericho.Properties.Resources.Sem_título;
+            this.btn_volumeMenos.Location = new System.Drawing.Point(60, 445);
             this.btn_volumeMenos.Name = "btn_volumeMenos";
-            this.btn_volumeMenos.Size = new System.Drawing.Size(69, 48);
+            this.btn_volumeMenos.Size = new System.Drawing.Size(193, 189);
             this.btn_volumeMenos.TabIndex = 5;
-            this.btn_volumeMenos.Text = "Volume -";
-            this.btn_volumeMenos.UseVisualStyleBackColor = true;
+            this.btn_volumeMenos.UseVisualStyleBackColor = false;
             this.btn_volumeMenos.Click += new System.EventHandler(this.btn_volumeMenos_Click);
+            // 
+            // btn_volumeMais
+            // 
+            this.btn_volumeMais.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_volumeMais.ForeColor = System.Drawing.Color.Gold;
+            this.btn_volumeMais.Image = global::Jericho.Properties.Resources.Sem_título623;
+            this.btn_volumeMais.Location = new System.Drawing.Point(319, 445);
+            this.btn_volumeMais.Name = "btn_volumeMais";
+            this.btn_volumeMais.Size = new System.Drawing.Size(193, 193);
+            this.btn_volumeMais.TabIndex = 4;
+            this.btn_volumeMais.UseVisualStyleBackColor = false;
+            this.btn_volumeMais.Click += new System.EventHandler(this.btn_volumeMais_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.ForeColor = System.Drawing.Color.Gold;
+            this.btn_stop.Image = global::Jericho.Properties.Resources.Sem_título444;
+            this.btn_stop.Location = new System.Drawing.Point(593, 445);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(193, 189);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_pause.ForeColor = System.Drawing.Color.Gold;
+            this.btn_pause.Image = global::Jericho.Properties.Resources.Sem_título555;
+            this.btn_pause.Location = new System.Drawing.Point(866, 445);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(193, 189);
+            this.btn_pause.TabIndex = 2;
+            this.btn_pause.UseVisualStyleBackColor = false;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Image = global::Jericho.Properties.Resources.black_youtube_logo_pngd1e_4b74_b2e7_745c6b3c0add;
+            this.btn_Play.Location = new System.Drawing.Point(1118, 445);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(193, 189);
+            this.btn_Play.TabIndex = 0;
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
             // 
             // Player_Mp3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1250, 507);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_volumeMenos);
             this.Controls.Add(this.btn_volumeMais);
             this.Controls.Add(this.btn_stop);
@@ -133,7 +153,8 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Button btn_pause;
         private System.Windows.Forms.Button btn_stop;
-        private System.Windows.Forms.Button btn_volumeMais;
         private System.Windows.Forms.Button btn_volumeMenos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_volumeMais;
     }
 }
--- jericho/Jericho/mp3_pagina.Designer.cs
+++ jericho/Jericho/mp3_pagina.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.NOME = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -37,6 +37,8 @@
             this.GENERO = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DATA = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -52,8 +54,8 @@
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -61,7 +63,7 @@
             this.ARTISTA,
             this.GENERO,
             this.DATA});
-            this.dataGridView1.Location = new System.Drawing.Point(146, 338);
+            this.dataGridView1.Location = new System.Drawing.Point(149, 409);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(996, 359);
             this.dataGridView1.TabIndex = 3;
@@ -69,8 +71,8 @@
             // 
             // NOME
             // 
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NOME.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NOME.DefaultCellStyle = dataGridViewCellStyle2;
             this.NOME.FillWeight = 250F;
             this.NOME.HeaderText = "NOME DA MUSICA";
             this.NOME.Name = "NOME";
@@ -96,18 +98,37 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(1003, 283);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(434, 343);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(251, 42);
+            this.button2.Size = new System.Drawing.Size(383, 42);
             this.button2.TabIndex = 4;
             this.button2.Text = "BUSCAR";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::Jericho.Properties.Resources.Sem_Título_3;
+            this.button5.Location = new System.Drawing.Point(69, 250);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 135);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::Jericho.Properties.Resources.gettyimages_663936454_170x170;
+            this.button4.Location = new System.Drawing.Point(1051, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 168);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Image = global::Jericho.Properties.Resources.YouTube_Music_Icon_16x9_375x267;
+            this.button3.Image = global::Jericho.Properties.Resources.gettyimages_663936454_170x170;
             this.button3.Location = new System.Drawing.Point(246, 57);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(159, 168);
@@ -116,7 +137,7 @@
             // 
             // button1
             // 
-            this.button1.Image = global::Jericho.Properties.Resources.avatars_000356896430_cvwzvd_t500x5001;
+            this.button1.Image = global::Jericho.Properties.Resources.avatars_000356896430_cvwzvd_t500x500;
             this.button1.Location = new System.Drawing.Point(476, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(524, 156);
@@ -129,6 +150,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1295, 606);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
@@ -153,5 +176,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
